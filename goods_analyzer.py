02627b4ca57d5aa508b6858f22018454db8b98d7
Maxim Kovalev <maxim.kovalev@west.cmu.edu>
2015-03-25 22:51:47 -0700

    ...

---
+++ goods_analyzer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import re
+import json
+import pickle
+
+def main():
+	f = open("/mnt/data/twitterdump/dump_mar_24_22_50", "r")
+	keywords = re.compile("( rain|storm|cloud| thunder|hurricane|tornado| hail| snow|flood| snow)", re.IGNORECASE)
+	pattern = re.compile("coordinates")
+	retweeted = re.compile("retweeted_status")
+	goods = []
+	for line in f:
+		try:
+			parsed = json.loads(line)
+			text = parsed["text"]
+			coordinates = parsed['retweeted_status']['coordinates']
+			if not coordinates:
+				continue
+			if keywords.search(text):
+				print coordinates, ":", text, '\n'
+				goods.append(parsed)
+		except Exception as e:
+			pass
+
+	f.close()
+	f = open("/mnt/data/twitterdump/relevant_tweets", "wb")
+	pickle.dump(goods, f)
+	f.close()
+
+if __name__ == "__main__":
+	main()
+
